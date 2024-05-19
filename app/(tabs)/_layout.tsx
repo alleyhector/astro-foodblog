bb2f98c334137ee781eec23ab7c8829ea43e35a5
Alley Hector <alley@chaninicholas.com>
2024-05-19 15:39:49 -0700

    Update settings stuff: icons, splash, app.json info, material icons etc

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import FontAwesome from '@expo/vector-icons/FontAwesome'
 import { Link, Tabs } from 'expo-router'
 import { Pressable } from 'react-native'
-
 import Colors from '@/constants/Colors'
 import { useColorScheme } from '@/components/useColorScheme'
 import { useClientOnlyValue } from '@/components/useClientOnlyValue'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name']
+  name: React.ComponentProps<typeof MaterialCommunityIcons>['name']
   color: string
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />
+  return (
+    <MaterialCommunityIcons size={28} style={{ marginBottom: -3 }} {...props} />
+  )
 }
 
 export default function TabLayout() {
@@ -31,20 +32,8 @@ export default function TabLayout() {
         name='index'
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
-          headerRight: () => (
-            <Link href='/modal' asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name='info-circle'
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name='zodiac-taurus' color={color} />
           ),
         }}
       />
@@ -52,14 +41,18 @@ export default function TabLayout() {
         name='archive'
         options={{
           title: 'Archive',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name='zodiac-virgo' color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name='about'
         options={{
           title: 'About',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name='zodiac-cancer' color={color} />
+          ),
         }}
       />
     </Tabs>
